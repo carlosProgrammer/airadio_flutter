58ed5f5dca853b3d0880464de397244c336bc623
cpinto@toxiostek <K4m1s4m4$>
2021-06-22 11:47:33 -0400

    Fixed nullable data error

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:airadio/model/radio.dart';
 import 'package:airadio/utils/ai_util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,11 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<MyRadio> radios;
+
   @override
   void initState() {
     super.initState();
+    fetchRadios();
   }
-  
+
+  fetchRadios() async {
+    final radioJson = await rootBundle.loadString("assets/radio.json");
+    radios = MyRadioList.fromJson(radioJson).radios!;
+    print(radios);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
